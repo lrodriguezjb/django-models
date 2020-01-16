6d9384afd1ec7890408b4e328e2620c61c9b20b2
Leandro Rodriguez <lrodriguezjb@gmail.com>
2020-01-16 01:52:58 -0800

    full crud working

--- krazapp/urls.py
+++ krazapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import HomePage, PostDetailView
+from .views import HomePage, PostDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 
 urlpatterns = [
     path('',HomePage.as_view(), name = 'home'),
     path('post/<int:pk>',PostDetailView.as_view(), name='posts_detail'),
+    path('post/new/', BlogCreateView.as_view(), name = 'post_new'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name = 'post_edit'),
+    path('post/delete/<int:pk>', BlogDeleteView.as_view(), name='delete_post'),
+
 ]
